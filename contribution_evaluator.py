ff8f89a34f7ce271d0ca5775286d8893ebda2f00
SeoJeongYeop <sjyswe99@gmail.com>
2021-10-28 20:52:41 +0900

    feat: save data of repo statistic - save indie, team, and total repo data - separate executor file

--- contribution_evaluator.py
+++ contribution_evaluator.py
@@ -17,8 +17,8 @@ class Contributor:
         self.followings=stats["followings"] 
         self.total_repos=stats["total_repos"] 
         self.total_commits = stats["total_commits"]
-        self.total_PRs = stats["total_PRs"]
-        self.total_issues = stats["total_issues"]
+        self.total_PRs = int(stats["total_PRs"])
+        self.total_issues = int(stats["total_issues"])
         self.achievements = stats["achievements"]
         self.highlights = stats["highlights"]
         self.indie_repositories = []
@@ -85,24 +85,25 @@ class Contributor:
         indie_prs_count = 0
         indie_open_issue_count = 0
         indie_close_issue_count = 0
-        indie_wachers_count = 0
+        indie_watchers_count = 0
         indie_dependencies = 0
-        indie_contributor_count = 0
+        indie_contributors_count = 0
         indie_release_count = 0
         indie_readme = 0
         for indie in self.indie_repositories:
-            indie_stargazers_count += indie["stargazers_count"]
-            indie_forks_count += indie["stargazers_count"]
-            indie_commits_count += indie["stargazers_count"]
-            indie_code_edits += indie["stargazers_count"]
-            indie_prs_count += indie["stargazers_count"]
-            indie_open_issue_count += indie["stargazers_count"]
-            indie_close_issue_count += indie["stargazers_count"]
-            indie_wachers_count += indie["stargazers_count"]
-            indie_dependencies += indie["stargazers_count"]
-            indie_contributor_count += indie["stargazers_count"]
-            indie_release_count += indie["stargazers_count"]
-            indie_readme += indie["stargazers_count"]
+            print("indie ",indie.commits_count)
+            indie_stargazers_count += indie.stargazers_count
+            indie_forks_count += indie.forks_count
+            indie_commits_count += indie.commits_count
+            indie_code_edits += indie.code_edits
+            indie_prs_count += indie.prs_count
+            indie_open_issue_count += indie.open_issue_count
+            indie_close_issue_count += indie.close_issue_count
+            indie_watchers_count += indie.watchers_count
+            indie_dependencies += indie.dependencies
+            indie_contributors_count += indie.contributors_count
+            indie_release_count += indie.release_count
+            indie_readme += indie.readme
 
             self.indie_stargazers_count = indie_stargazers_count
             self.indie_forks_count = indie_forks_count
@@ -111,12 +112,11 @@ class Contributor:
             self.indie_prs_count = indie_prs_count
             self.indie_open_issue_count = indie_open_issue_count
             self.indie_close_issue_count = indie_close_issue_count
-            self.indie_wachers_count = indie_wachers_count
+            self.indie_watchers_count = indie_watchers_count
             self.indie_dependencies = indie_dependencies
-            self.indie_contributor_count = indie_contributor_count
+            self.indie_contributors_count = indie_contributors_count
             self.indie_release_count = indie_release_count
             self.indie_readme = indie_readme
-
         team_stargazers_count = 0
         team_forks_count = 0
         team_commits_count = 0
@@ -124,24 +124,24 @@ class Contributor:
         team_prs_count = 0
         team_open_issue_count = 0
         team_close_issue_count = 0
-        team_wachers_count = 0
+        team_watchers_count = 0
         team_dependencies = 0
-        team_contributor_count = 0
+        team_contributors_count = 0
         team_release_count = 0
         team_readme = 0
         for team in self.team_repositories:
-            team_stargazers_count += team["stargazers_count"]
-            team_forks_count += team["stargazers_count"]
-            team_commits_count += team["stargazers_count"]
-            team_code_edits += team["stargazers_count"]
-            team_prs_count += team["stargazers_count"]
-            team_open_issue_count += team["stargazers_count"]
-            team_close_issue_count += team["stargazers_count"]
-            team_wachers_count += team["stargazers_count"]
-            team_dependencies += team["stargazers_count"]
-            team_contributor_count += team["stargazers_count"]
-            team_release_count += team["stargazers_count"]
-            team_readme += team["stargazers_count"]
+            team_stargazers_count += team.stargazers_count
+            team_forks_count += team.forks_count
+            team_commits_count += team.commits_count
+            team_code_edits += team.code_edits
+            team_prs_count += team.prs_count
+            team_open_issue_count += team.open_issue_count
+            team_close_issue_count += team.close_issue_count
+            team_watchers_count += team.watchers_count
+            team_dependencies += team.dependencies
+            team_contributors_count += team.contributors_count
+            team_release_count += team.release_count
+            team_readme += team.readme
         self.team_stargazers_count = team_stargazers_count
         self.team_forks_count = team_forks_count
         self.team_commits_count = team_commits_count
@@ -149,11 +149,61 @@ class Contributor:
         self.team_prs_count = team_prs_count
         self.team_open_issue_count = team_open_issue_count
         self.team_close_issue_count = team_close_issue_count
-        self.team_wachers_count = team_wachers_count
+        self.team_watchers_count = team_watchers_count
         self.team_dependencies = team_dependencies
-        self.team_contributor_count = team_contributor_count
+        self.team_contributors_count = team_contributors_count
         self.team_release_count = team_release_count
         self.team_readme = team_readme
+
+    def getTotalContribution(self):
+        result = dict()
+        result["stars"] = self.stars
+        result["followers"] = self.followers
+        result["followings"] = self.followings
+        result["total_repos"] = self.total_repos
+        result["total_commits"] = self.total_commits
+        result["total_PRs"] = self.total_PRs
+        result["total_issues"] = self.total_issues
+        result["achievements"] = self.achievements
+        result["highlights"] = self.highlights
+        result["indie_stargazers_count"] = self.indie_stargazers_count
+        result["indie_forks_count"] = self.indie_forks_count
+        result["indie_commits_count"] = self.indie_commits_count
+        result["indie_code_edits"] = self.indie_code_edits
+        result["indie_prs_count"] = self.indie_prs_count
+        result["indie_open_issue_count"] = self.indie_open_issue_count
+        result["indie_close_issue_count"] = self.indie_close_issue_count
+        result["indie_wachers_count"] = self.indie_wachers_count
+        result["indie_dependencies"] = self.indie_dependencies
+        result["indie_contributor_count"] = self.indie_contributor_count
+        result["indie_release_count"] = self.indie_release_count
+        result["indie_readme"] = self.indie_readme
+        result["team_stargazers_count"] = self.team_stargazers_count
+        result["team_forks_count"] = self.team_forks_count
+        result["team_commits_count"] = self.team_commits_count
+        result["team_code_edits"] = self.team_code_edits
+        result["team_prs_count"] = self.team_prs_count
+        result["team_open_issue_count"] = self.team_open_issue_count
+        result["team_close_issue_count"] = self.team_close_issue_count
+        result["team_wachers_count"] = self.team_wachers_count
+        result["team_dependencies"] = self.team_dependencies
+        result["team_contributor_count"] = self.team_contributor_count
+        result["team_release_count"] = self.team_release_count
+        result["team_readme"] = self.team_readme
+        result["total_stargazers_count"] = self.indie_stargazers_count+self.team_stargazers_count
+        result["total_forks_count"] = self.indie_forks_count+self.team_forks_count
+        result["total_commits_count"] = self.indie_commits_count+self.team_commits_count
+        result["total_code_edits"] = self.indie_code_edits+self.team_code_edits
+        result["total_prs_count"] = self.indie_prs_count+self.team_prs_count
+        result["total_open_issue_count"] = self.indie_open_issue_count+self.team_open_issue_count
+        result["total_close_issue_count"] = self.indie_close_issue_count+self.team_close_issue_count
+        result["total_wachers_count"] = self.indie_wachers_count+self.team_wachers_count
+        result["total_dependencies"] = self.indie_dependencies+self.team_dependencies
+        result["total_contributor_count"] = self.indie_contributor_count+self.team_contributor_count
+        result["total_release_count"] = self.indie_release_count+self.team_release_count
+        result["total_readme"] = self.indie_readme+self.team_readme
+        
+        return result 
     
         
 class Repository:
@@ -241,7 +291,28 @@ def saveJson(tableName:str):
             jsonfile.write(json.dumps(result, ensure_ascii=False))
         except Exception as e:
             print(e)
-            
+def saveTotalJson(contributorDict):
+    # with open("github_overview.json",'r', encoding="utf-8") as repo:
+    #     repo_overview_data = json.load(repo)
+    print("saveTotalJson fucntion")
+    ## 1. sum repo statistic
+    print("sum repo statistic")
+    for contributor in contributorDict:
+        print(contributor, " sum start...")
+        contributorDict[contributor].sumContribution()
+    ## 2. save total statistic
+    result = dict()
+    print("save total statistic")
+    for contributor in contributorDict:
+        result[contributor] = contributorDict[contributor].getTotalContribution()
+
+    with open(f"./github_statistic.json", 'w', encoding="utf-8") as jsonfile:
+        try:
+            jsonfile.write(json.dumps(result, ensure_ascii=False))
+        except Exception as e:
+            print(e)
+    print("done!!")
+        
 def makeContributor():
     print("make contributor")
     with open("github_overview.json",'r', encoding="utf-8") as overview:
@@ -264,7 +335,6 @@ def linkRepo(contributorDict:dict):
                 contributorDict[repo_stats["github_id"]].link_indie_repo(repo_stats)
             else :
                 team = 0
-                #contributorDict[repo_stats["github_id"]].link_team_repo(repo_stats)
                 print("find other contributor")
                 for repo_contributor in repo_contributor_data:
                     if repo_contributor["repo_name"] == repo_stats["repo_name"] :
@@ -275,63 +345,9 @@ def linkRepo(contributorDict:dict):
                 print(str(team) + " compare more")
 
         except Exception as e:
-            #print(f"{repo_stats['github_id']} is ...")
-            print(e)
+            print(f"{repo_stats['repo_name']} has error", e)
             exceptList.append(repo_stats)
             continue
     with open("github_repo_except.json",'w', encoding="utf-8") as expt:
         expt.write(json.dumps(exceptList, ensure_ascii=False))
     return contributorDict
-
-if __name__ == "__main__":
-    contributorDict = {}
-    while(1):
-        print("1: Fetch all table and save json file")
-        print("2: Fetch input table and save json file")
-        print("3: Json make contributor object")
-        print("4: Contributor link repository data")
-        print("5: Merge Json data and save json file")
-        print("8: Show contributor data")
-        print("9: quit")
-        num = input(">>>")
-        if(num=='1'):
-            try :
-                tableNameList = ["github_overview","github_repo_contributor","github_repo_stats", "github_repo_stats_yymm","github_stats_yymm","student_tab"]
-                for tableName in tableNameList :
-                    saveJson(tableName)
-            except Exception as e:
-                print("error1: ",e)
-        if(num=='2'):
-            try :
-                print("Input table name:")
-                tableName = input(">>>")
-                saveJson(tableName)
-            except Exception as e:
-                print("error2: ",e)
-        elif(num=='3'):
-            try:
-                contributorDict = makeContributor()
-            except Exception as e:
-                print("error3: ",e)
-        elif(num=='4'):
-            try:
-                contributorDict = linkRepo(contributorDict)
-            except Exception as e:
-                print("error4: ",e)
-        elif(num=='5'):
-            try:
-                contributorDict = linkRepo(contributorDict)
-            except Exception as e:
-                print("error5: ",e)
-        elif(num=='8'):
-            try:
-                print("input contributor name")
-                name = input(">>>")
-                contributorDict[name].showContribution()
-            except Exception as e:
-                print("error8: ",e)
-        elif(num=='9'):
-            print("Bye!")
-            break
-        else:
-            continue
---
+++ executor.py
@@ -0,0 +1,54 @@
+import contribution_evaluator as cv
+
+if __name__ == "__main__":
+    contributorDict = {}
+    while(1):
+        print("1: Fetch all table and save json file")
+        print("2: Fetch input table and save json file")
+        print("3: Json make contributor object")
+        print("4: Contributor link repository data")
+        print("5: Merge Json data and save json file")
+        print("8: Show contributor data")
+        print("9: quit")
+        num = input(">>>")
+        if(num=='1'):
+            try :
+                tableNameList = ["github_overview","github_repo_contributor","github_repo_stats", "github_repo_stats_yymm","github_stats_yymm","student_tab"]
+                for tableName in tableNameList :
+                    cv.saveJson(tableName)
+            except Exception as e:
+                print("error1: ",e)
+        if(num=='2'):
+            try :
+                print("Input table name:")
+                tableName = input(">>>")
+                cv.saveJson(tableName)
+            except Exception as e:
+                print("error2: ",e)
+        elif(num=='3'):
+            try:
+                contributorDict = cv.makeContributor()
+            except Exception as e:
+                print("error3: ",e)
+        elif(num=='4'):
+            try:
+                contributorDict = cv.linkRepo(contributorDict)
+            except Exception as e:
+                print("error4: ",e)
+        elif(num=='5'):
+            try:
+                contributorDict = cv.saveTotalJson(contributorDict)
+            except Exception as e:
+                print("error5: ",e)
+        elif(num=='8'):
+            try:
+                print("input contributor name")
+                name = input(">>>")
+                contributorDict[name].showContribution()
+            except Exception as e:
+                print("error8: ",e)
+        elif(num=='9'):
+            print("Bye!")
+            break
+        else:
+            continue
