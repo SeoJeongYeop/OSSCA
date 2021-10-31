ed02b665eb238ea28e3d3b4b69d23e6f4753ac8e
SeoJeongYeop <sjyswe99@gmail.com>
2021-10-31 22:14:43 +0900

    refactor: Change instructions order

--- executor.py
+++ executor.py
@@ -3,68 +3,99 @@ import contribution_evaluator as cv
 if __name__ == "__main__":
     contributorDict = {}
     while(1):
-        print("1: Fetch all table and save json file")
-        print("2: Fetch input table and save json file")
-        print("3: Json make contributor object")
-        print("4: Contributor link repository data")
-        print("5: anlyze commits data")
-        print("6: Merge Json data and save json file")
-        
-        print("8: Show contributor data")
-        print("9: evaluate opensource score")
+        print("1: READ")
+        print("2: ANALYSIS")
         print("q: quit")
-        num = input(">>>")
-        if(num=='1'):
-            try :
-                tableNameList = ["github_overview","github_repo_commits","github_repo_contributor","github_repo_stats", "github_repo_stats_yymm","github_stats_yymm","student_tab"]
-                for tableName in tableNameList :
-                    cv.saveJson(tableName)
-            except Exception as e:
-                print("error1: ",e)
-        if(num=='2'):
-            try :
-                print("Input table name:")
-                tableName = input(">>>")
-                cv.saveJson(tableName)
-            except Exception as e:
-                print("error2: ",e)
-        elif(num=='3'):
-            try:
-                contributorDict = cv.makeContributor()
-            except Exception as e:
-                print("error3: ",e)
-        elif(num=='4'):
-            try:
-                contributorDict = cv.linkRepo(contributorDict)
-            except Exception as e:
-                print("error4: ",e)
-        elif(num=='5'):
-            try:
-                cv.analyzeCommits(contributorDict)
-            except Exception as e:
-                print("error5: ",e)
-        elif(num=='6'):
-            try:
-                cv.saveTotalJson(contributorDict)
-            except Exception as e:
-                print("error6: ",e)
+        mode = input(">>>")
         
-        elif(num=='8'):
-            try:
-                print("input contributor name")
-                name = input(">>>")
-                print("How should it print out? 1. Compact 2. Normal 3. Detail")
-                how = input(">>>")
-                contributorDict[name].showContribution(how)
-            except Exception as e:
-                print("error8: ",e)
-        elif(num=='9'):
-            try:
-                cv.yieldScore(contributorDict)
-            except Exception as e:
-                print("error9: ",e)
-        elif(num=='q'):
+        if mode=='1' :
+            while(1):
+                print("1: Fetch all table and save json file")
+                print("2: Fetch input table and save json file")
+                print("3: Make yearly data")
+                print("q: quit")
+                num = input(">>>")
+                if(num=='1'):
+                    try :
+                        tableNameList = ["github_overview","github_repo_commits","github_repo_contributor","github_repo_stats", "github_repo_stats_yymm","github_stats_yymm","student_tab"]
+                        for tableName in tableNameList :
+                            cv.saveJson(tableName)
+                    except Exception as e:
+                        print("error1: ",e)
+                elif(num=='2'):
+                    try :
+                        print("Input table name:")
+                        tableName = input(">>>")
+                        cv.saveJson(tableName)
+                    except Exception as e:
+                        print("error2: ",e)
+                elif(num=='3'):
+                    try :
+                        cv.savePeriodData()
+                    except Exception as e:
+                        print("error2: ",e)
+                elif(num=='q'):
+                    break
+        elif mode == '2' :
+            while(1) :
+                print("a: all process")
+                print("1: Json make contributor object")
+                print("2: Contributor link repository data")
+                print("3: anlyze commits data")
+                print("4: Merge Json data and save json file")
+                print("5: evaluate opensource score")
+
+                print("9: Show contributor data")
+                print("q: quit")
+                num = input(">>>")
+                if(num=='a'):
+                    try:
+                        contributorDict = cv.makeContributor()
+                        contributorDict = cv.linkRepo(contributorDict)
+                        cv.analyzeCommits(contributorDict)
+                        cv.saveTotalJson(contributorDict)
+                        cv.yieldScore(contributorDict)
+                        print("done!")
+                    except Exception as e:
+                        print("error a: ",e)
+                if(num=='1'):
+                    try:
+                        contributorDict = cv.makeContributor()
+                    except Exception as e:
+                        print("error1: ",e)
+                elif(num=='2'):
+                    try:
+                        contributorDict = cv.linkRepo(contributorDict)
+                    except Exception as e:
+                        print("error2: ",e)
+                elif(num=='3'):
+                    try:
+                        cv.analyzeCommits(contributorDict)
+                    except Exception as e:
+                        print("error3: ",e)
+                elif(num=='4'):
+                    try:
+                        cv.saveTotalJson(contributorDict)
+                    except Exception as e:
+                        print("error4: ",e)
+                elif(num=='5'):
+                    try:
+                        cv.yieldScore(contributorDict)
+                    except Exception as e:
+                        print("error5: ",e)
+                elif(num=='9'):
+                    try:
+                        print("input contributor name")
+                        name = input(">>>")
+                        print("How should it print out? 1. Compact 2. Normal 3. Detail")
+                        how = input(">>>")
+                        contributorDict[name].showContribution(how)
+                    except Exception as e:
+                        print("error9: ",e)
+                elif(num=='q'):
+                    break
+                else:
+                    continue
+        elif mode == 'q' :
             print("Bye!")
-            break
-        else:
-            continue
+            break;
