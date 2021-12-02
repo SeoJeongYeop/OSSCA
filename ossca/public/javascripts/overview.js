15f2c861495e633c6210e1ac533b3e5531910d55
tsfo1489 <tsfo1489@gmail.com>
2021-12-02 15:37:48 +0900

    fix: Fix user table page filter

--- ossca/public/javascripts/overview.js
+++ ossca/public/javascripts/overview.js
@@ -18,61 +18,61 @@ $(document).ready(function () {
             {
               label:'0 ~ 0.5',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 0 && Number(rowData[3]) <= 0.5
+                return Number(rowData[4]) > 0 && Number(rowData[4]) <= 0.5
               }
             },
             {
               label:'0.5 ~ 1.0',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 0.5 && Number(rowData[3]) <= 1.0
+                return Number(rowData[4]) > 0.5 && Number(rowData[4]) <= 1.0
               }
             },
             {
               label:'1.0 ~ 1.5',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 1.0 && Number(rowData[3]) <= 1.5
+                return Number(rowData[4]) > 1.0 && Number(rowData[4]) <= 1.5
               }
             },
             {
               label:'1.5 ~ 2.0',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 1.5 && Number(rowData[3]) <= 2.0
+                return Number(rowData[4]) > 1.5 && Number(rowData[4]) <= 2.0
               }
             },
             {
               label:'2.0 ~ 2.5',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 2.0 && Number(rowData[3]) <= 2.5
+                return Number(rowData[4]) > 2.0 && Number(rowData[4]) <= 2.5
               }
             },
             {
               label:'2.5 ~ 3.0',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 2.5 && Number(rowData[3]) <= 3.0
+                return Number(rowData[4]) > 2.5 && Number(rowData[4]) <= 3.0
               }
             },
             {
               label:'3.0 ~ 3.5',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 3.0 && Number(rowData[3]) <= 3.5
+                return Number(rowData[4]) > 3.0 && Number(rowData[4]) <= 3.5
               }
             },
             {
               label:'3.5 ~ 4.0',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 3.5 && Number(rowData[3]) <= 4.0
+                return Number(rowData[4]) > 3.5 && Number(rowData[4]) <= 4.0
               }
             },
             {
               label:'4.0 ~ 4.5',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 4.0 && Number(rowData[3]) <= 4.5
+                return Number(rowData[4]) > 4.0 && Number(rowData[4]) <= 4.5
               }
             },
             {
               label:'4.5 ~ 5.0',
               value: function(rowData, rowIdx){
-                return Number(rowData[3]) > 4.5 && Number(rowData[3]) <= 5.0
+                return Number(rowData[4]) > 4.5 && Number(rowData[4]) <= 5.0
               }
             }
           ]
